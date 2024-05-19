2c7a2556d508871ee936f655d25728eb9daf6a30
Florian Klein <florian.klein02@gmx.net>
2024-05-19 23:23:46 +0200

    tested lstm and fixed nan value error

--- main/foo.py
+++ main/foo.py
@@ -12,8 +12,6 @@ class LSTM(nn.Module):
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
-        self.optimizer = optim.Adam(self.parameters(), lr=0.0001)
-
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
