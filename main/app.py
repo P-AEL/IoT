b3e8697c8751136925f6063a451f57902443444c
Florian Klein <florian.klein02@gmx.net>
2024-05-19 23:24:06 +0200

    testing

--- main/app.py
+++ main/app.py
@@ -1,9 +1,9 @@
 import streamlit as st, pandas as pd, numpy as np, pyvista as pv
 import plotly.graph_objects as go
-from group_data import group_data
 from stpyvista import stpyvista
 from datetime import date
-from pyvista_test import plot_cube
+import dataprep as dp
+#from pyvista_test import plot_cube
 
 # Set page config
 st.set_page_config(
@@ -20,7 +20,7 @@ st.title("Sensor Dashboard Building A")
 
 # Load data
 data = pd.read_csv("output.csv")
-df_hour = group_data(data, "h")
+df_hour = dp.group_data(data, "h")
 
 # Sidebar
 st.sidebar.header("Sensor Dashboard Building A")
@@ -31,14 +31,14 @@ input_date = st.sidebar.date_input(label= "Select Date", value= date(2022,10,10)
 filtered_data = df_hour[(df_hour["device_id"].astype(str) == input_device) & (df_hour["date_time"].astype(str).str.slice(0, 10).str.contains(str(input_date)))]
 
 # Create plot
-def plot_figure(data, y):
+def plt_fig(data, y, mode="lines+markers"):
         fig = go.Figure()
         fig.add_trace(
             go.Scatter(
                 x=data["date_time"],
                 y=data[y],
                 name=y,
-                mode="lines+markers"
+                mode=mode
             )
         )
         fig.update_layout(xaxis_title= "Time", yaxis_title= y)
@@ -50,11 +50,13 @@ c1, c2 = st.columns(2)
 with c1:
 
     st.markdown("## Pauls Tolle Graphik")
-    plotter = plot_cube()
-    plotter.view_isometric()
-    plotter.add_scalar_bar()
-    plotter.background_color = "black"
-    stpyvista(plotter)
+    # plotter = plot_cube()
+    # plotter.view_isometric()
+    # plotter.add_scalar_bar()
+    # plotter.background_color = "black"
+    # stpyvista(plotter)
+
+
 with c2:
 
     kpi1, kpi2, kpi3 = st.columns(3)
@@ -78,18 +80,40 @@ with c2:
 
     with tab1:
         st.markdown("### Temperature in °C")
-        st.plotly_chart(plot_figure(filtered_data, "tmp"))
+        st.plotly_chart(plt_fig(filtered_data, "tmp"))
        
     with tab2:
         st.markdown("### Humidity in %")
-        st.plotly_chart(plot_figure(filtered_data, "hum"))
+        st.plotly_chart(plt_fig(filtered_data, "hum"))
 
     with tab3:
         st.markdown("### CO2 in ppm")
-        st.plotly_chart(plot_figure(filtered_data, "CO2"))
+        st.plotly_chart(plt_fig(filtered_data, "CO2"))
+
+
+# Gaps in the data and predictions
+# tmp_tab1, hum_tab2, co2_tab3 = st.tabs(["Temperature", "Humidity", "CO2"])
+
+# with tmp_tab1:
+#     st.markdown("### Temperature in °C")
+#     st.plotly_chart(plt_fig(df_hour, "tmp", "markers"))
+    
+# with hum_tab2:
+#     st.markdown("### Humidity in %")
+#     st.plotly_chart(plt_fig(df_hour, "hum", "markers"))
+
+# with co2_tab3:
+#     st.markdown("### CO2 in ppm")
+#     st.plotly_chart(plt_fig(df_hour, "CO2", "markers"))
+
 
 # Detailed data view
 st.markdown("## Detailed Data View") 
 st.dataframe(filtered_data)
 
 # detail view max min mean 
+
+
+
+# raum102 wirft fhler auf 
+#AttributeError: 'float' object has no attribute 'round'
