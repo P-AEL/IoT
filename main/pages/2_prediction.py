b737fddb640291cd2bfc890554ba7e77265d6a52
Florian Klein <florian.klein02@gmx.net>
2024-06-22 18:22:01 +0200

    chore: Remove unnecessary imports, format code, and correct inner quotation marks

--- main/pages/2_prediction.py
+++ main/pages/2_prediction.py
@@ -20,37 +20,45 @@ st.set_page_config(
 
 FILEPATH_INFLUXDB_CREDENTIALS = "/Users/florian/Documents/github/study/IoT/IoT/credentials.json"
 
-
 # Functions
-def read_credentials(filename: str= "credentials.json"):
+def read_credentials(
+        filename: str= "credentials.json"
+        ) -> dict:
     """
     Reads the credentials from a given filename.
 
-    args:   filename: str
     returns: dict
     """
-    print("filename", filename)
-    with open(filename, "r") as file:
-        credentials = json.load(file)
+    try:
+        with open(filename, "r") as file:
+            credentials = json.load(file)
+
+    except FileNotFoundError:
+        logging.error(f"Could not find file {filename}.")
+        raise 
+    
     return credentials
 
-def make_tz_naive(timestamp):
+def make_tz_naive(
+        timestamp
+    ):
     """
     Makes a timestamp timezone naive.
 
-    args:   timestamp: timestamp
     returns: timestamp
     """
     if timestamp.tzinfo is not None:
         return timestamp.tz_convert(None).tz_localize(None)
+
     return timestamp
 
-def extract_data_from_influxdb(credentials, query) -> list:
+def extract_data_from_influxdb(
+        credentials: dict,
+        query: str,
+    ) -> list:
     """
     Create client and instantiate read/write API
 
-    args:   credentials: dict
-            query: str
     returns: list
     """
     write_client = influxdb_client.InfluxDBClient(url= credentials['url'], token= credentials['token'], org= credentials['org'])
@@ -60,25 +68,25 @@ def extract_data_from_influxdb(credentials, query) -> list:
     data = []
     for table in tables:
         for record in table.records:
-            # Extract the necessary fields
             time = record.get_time()
             value = record.get_value()
             measurement = record.values.get('_measurement')
             field = record.values.get('_field')
             sensor = record.values.get('sensor')
-            
-            # Append to data list
             data.append([time, value, measurement, field, sensor])
+
     return data
 
 @st.cache_data
-def load_data(filename: str= "agg_hourly.parquet", use_influx_db: bool= False) -> pd.DataFrame:
+def load_data(
+        filename: str= "agg_hourly.parquet",
+        use_influx_db: bool= False
+    ) -> pd.DataFrame:
     """
     Loads data from given filename or InfluxDB.
     
-    args:   filename: str
-            use_influx_db: bool
-    returns pd.DataFrame
+    returns: pd.DataFrame
+    raises: FileNotFoundError if not able to load the file.
     """
     df_new = []
 
@@ -102,17 +110,19 @@ def load_data(filename: str= "agg_hourly.parquet", use_influx_db: bool= False) -
             row = [time]
             for field in fields:        
                 row.append(df_time[df_time['measurement'].str.contains(field)]['value'].tolist()[0])
-
             df_new.append(row)
 
         df_new_room_014 = pd.DataFrame(df_new, columns=['date_time'] + fields)
         df_new_room_014['date_time'] = df_new_room_014['date_time'].apply(make_tz_naive)
+
         try:
             filepath = os.path.join("./data/aggregated_data/", filename)
             df_new_without_room_014 = pd.read_parquet(filepath)
+
         except FileNotFoundError:
             logging.error(f"File {filepath} does not exist.")
             raise
+
         df_new_without_room_014 = df_new_without_room_014[df_new_without_room_014['device_id'] != 'a014']
         df_new = pd.concat([df_new_room_014, df_new_without_room_014], ignore_index=True)
         df_new.date_time = pd.to_datetime(df_new.date_time)
@@ -123,6 +133,7 @@ def load_data(filename: str= "agg_hourly.parquet", use_influx_db: bool= False) -
     try:
         filepath = os.path.join("./data/aggregated_data/", filename)
         df_new = pd.read_parquet(filepath)
+
     except FileNotFoundError:
         logging.error(f"File {filepath} does not exist.")
         raise
@@ -130,13 +141,15 @@ def load_data(filename: str= "agg_hourly.parquet", use_influx_db: bool= False) -
     return df_new
 
 @st.cache_resource
-def load_model(model_name: str, device: torch.device):
+def load_model(
+        model_name: str,
+        device: torch.device
+    ) -> torch.nn.Module:
     """
     Load the pre-trained model from the models directory.
 
-    args:   model_name: str
-            device: torch.device
-    returns:    torch.nn.Module
+    returns: torch.nn.Module
+    raises: FileNotFoundError if not able to load the model file.
     """
     filename = f"{model_name}.pth"
     filepath = os.path.join("./main/models/", filename)
@@ -157,15 +170,17 @@ def load_model(model_name: str, device: torch.device):
 
     return model
 
-def update_features_and_targets(i, test_features, test_targets, horizon_dict, device, beta) -> tuple:
+def update_features_and_targets(
+        i: int, 
+        test_features: torch.tensor, 
+        test_targets: torch.tensor, 
+        horizon_dict: dict, 
+        device: torch.device, 
+        beta: float = 0.8
+    ) -> tuple:
     """
     Updates the features and targets for the next prediction step.
 
-    args:   i: int
-            test_features: torch.tensor
-            test_targets: torch.tensor
-            horizon_dict: dict
-            device: torch.device
     returns: tuple
     """
     if i > 0:
@@ -177,26 +192,29 @@ def update_features_and_targets(i, test_features, test_targets, horizon_dict, de
             if previous_prediction.dim() == 0:
                 previous_prediction = previous_prediction.unsqueeze(0)
 
-            new_feature = torch.cat((previous_prediction, (beta * test_feature[-1][1:] + (beta-1) * test_feature[-2][1:] + np.random.normal(0, 0.1))), dim=0).unsqueeze(0)
+            new_feature = torch.cat((previous_prediction, (beta * test_feature[-1][1:] + (1 - beta) * test_feature[-2][1:] + np.random.normal(0, 0.1))), dim=0).unsqueeze(0)
             test_features[idx] = torch.cat((test_feature, new_feature), dim=0)
         test_targets = torch.cat((test_targets[i:].to(device), torch.zeros(i, test_targets.shape[1]).to(device)), dim=0)
 
     return test_features.to(device), test_targets.to(device)
 
 @st.cache_data
-def create_Prediction(filepath: str= "agg_hourly.parquet", model_name: str= "LSTM", scaling: bool= True, target: str= "tmp", features: list=["tmp", "hum", "VOC", "CO2"], device_ids: list= ["a017", "a014", "a101", "a102", "a103", "a106", "a107", "a108", "a111", "a112"], start_date: str= "", horizon_step: int= 1, beta: float= 0.8) -> list:
+def create_Prediction(
+        filepath: str = "agg_hourly.parquet",
+        model_name: str = "LSTM",
+        scaling: bool = True,
+        target: str = "tmp",
+        features: list = ["tmp", "hum", "VOC", "CO2"],
+        device_ids: list = ["a017", "a014", "a101", "a102", "a103", "a106", "a107", "a108", "a111", "a112"],
+        start_date: str = "",
+        horizon_step: int = 1,
+        beta: float = 0.8
+    ) -> list:
     """
     Create a prediction from a pre-trained model for a given time range.
 
-    args:   filepath: str
-            model: str
-            scaling: bool
-            target: str
-            features: list
-            device_ids: list
-            start_date: str
-            horizon_step: int
     returns: list
+    raises: FileNotFoundError if not able to load the data or the model files and logging.error if not data is available for the selected date range.
     """
     data = load_data(filename= filepath)
     df = deepcopy(data)
@@ -214,11 +232,15 @@ def create_Prediction(filepath: str= "agg_hourly.parquet", model_name: str= "LST
 
         if scaling:
             scaler = StandardScaler()
-            df = scaler.fit_transform(df)
+            df_scaled = scaler.fit_transform(df)
 
-        X = dp.format_tensor(torch.tensor(df[:, :-1], dtype= torch.float32), 50)
-        y = torch.tensor(df[:-1, -1], dtype= torch.float32).view(-1, 1)
+            X = dp.format_tensor(torch.tensor(df_scaled[:, :-1], dtype= torch.float32), 50)
+            y = torch.tensor(df_scaled[:-1, -1], dtype= torch.float32).view(-1, 1)
         
+        else:
+            X = dp.format_tensor(torch.tensor(df.iloc[:, :-1].values, dtype=torch.float32), 50)
+            y = torch.tensor(df.iloc[:-1, -1].values, dtype=torch.float32).view(-1, 1)
+
         data_loader = DataLoader(TensorDataset(X, y), shuffle=False, batch_size=64) 
         test_features, test_targets = next(iter(data_loader))
 
@@ -242,22 +264,25 @@ def create_Prediction(filepath: str= "agg_hourly.parquet", model_name: str= "LST
             feature_scaler.mean_ = scaler.mean_[feature_index]
             feature_scaler.scale_ = scaler.scale_[feature_index]
 
-            targets = feature_scaler.inverse_transform(test_targets.to("cpu").numpy().reshape(-1, 1))
+            test_targets = feature_scaler.inverse_transform(test_targets.to("cpu").numpy().reshape(-1, 1))
             predictions = feature_scaler.inverse_transform(horizon_dict[horizon_step-1].to("cpu").numpy().reshape(-1, 1))
             
-            test_loss = nn.L1Loss()(torch.from_numpy(predictions[:-horizon_step]), torch.from_numpy(targets[:-horizon_step]))
+            test_loss = nn.L1Loss()(torch.from_numpy(predictions[:-horizon_step]), torch.from_numpy(test_targets[:-horizon_step]))
         
-        output = [predictions, targets, test_loss]
+        output = [predictions, test_targets, test_loss]
 
     return output
 
 @st.cache_data
-def create_ensemble(model_predictions, targets) -> list:
+def create_ensemble(
+        model_predictions: list,
+        targets: np.ndarray
+    ) -> list:
     """
     Create an ensemble prediction from the LSTM and the Transformer model.
 
     args: model_predictions: list of numpy arrays or tensors
-    return: tensor
+    returns: list of tensors
     """
     predictions_np = [pred.numpy() if isinstance(pred, torch.Tensor) else pred for pred in model_predictions]
     predictions_np = [pred.reshape(-1, 1) for pred in predictions_np]
@@ -269,15 +294,21 @@ def create_ensemble(model_predictions, targets) -> list:
 
     return [ensemble, ensemble_loss]
 
-def plot_predictions(x, targets, predictions, loss):
+def plot_predictions(
+        x,
+        targets, 
+        predictions, 
+        loss
+    ):
     """
     Plot the predictions and the targets.
 
     args:   x: pd.DatetimeIndex
-            targets: torch.tensor
-            predictions: torch.tensor
+            targets: torch.tensor if not scaling else np.ndarray
+            predictions: torch.tensor if not scaling else np.ndarray
             loss: torch.tensor
     return: st.plotly_chart, st.write
+    raises: logging.error if no data is available for the selected date range.
     """
     targets_size = targets.size if isinstance(targets, np.ndarray) else targets.numel()
     predictions_size = predictions.size if isinstance(predictions, np.ndarray) else predictions.numel()
@@ -298,14 +329,13 @@ def plot_predictions(x, targets, predictions, loss):
         st.plotly_chart(fig, use_container_width=True)
         st.write("loss in °C:", loss.item())
 
-@st.cache_data
-def prepare_data(input_device, df, key_prefix) -> pd.DataFrame:
+def prepare_data(
+        input_device: str,
+        df: pd.DataFrame,
+        key_prefix: str
+    ) -> pd.DataFrame:
     """
     Groups the data for the selected device.
-
-    args:   input_device: str
-            df: pd.DataFrame
-            key_prefix: str
     returns: pd.DataFrame
     """
     df_gaps = dp.build_lvl_df(df, a0 + a1, output_cols=OUTPUT_COLS, reset_ind=False).reset_index(drop=False) if input_device == "all" else df[(df["device_id"].astype(str) == input_device)]
@@ -345,8 +375,6 @@ FILENAME = "agg_hourly.parquet"
 data = load_data(FILENAME, use_influx_db= input_use_influx_db_data)
 df = deepcopy(data)
 
-
-
 # Page Content
 tmp_tab1, tab_pred = st.tabs(["Tmp gaps", "Tmp pred"])
 
@@ -368,17 +396,21 @@ with tmp_tab1:
 
 with tab_pred:
 
-    input_pred_model = st.selectbox(label= "Select Model", options= ["LSTM", "Transformer", "Ensemble"], index= 0)
-
-    input_pred_start_date = st.date_input(label= "Select Start Date", value= date(2023,9,4), min_value= data["date_time"].min().date(), max_value= data["date_time"].max().date())
-    input_pred_start_time = st.time_input(label= "Select Start Time", value= time(0, 0))
-    input_pred_start_datetime = datetime.combine(input_pred_start_date, input_pred_start_time)
+    col_left, col_right = st.columns(2)
     
-    input_pred_step_size = st.number_input(label= "Select Horizon Step", value= 2, min_value= 1, max_value= 4)
-    input_beta = st.number_input(label= "Select Beta", value= 0.8, min_value= 0.1, max_value= 1.0, step= 0.1)
+    with col_left:
+        input_pred_model = st.selectbox(label= "Select Model", options= ["LSTM", "Transformer", "Ensemble"], index= 0)
+        input_pred_start_date = st.date_input(label= "Select Start Date", value= date(2023,9,4), min_value= data["date_time"].min().date(), max_value= data["date_time"].max().date())
+        input_pred_start_time = st.time_input(label= "Select Start Time", value= time(0, 0))
+        input_pred_start_datetime = datetime.combine(input_pred_start_date, input_pred_start_time)
+    
+    with col_right:
+        input_pred_scaling = st.selectbox(label= "Select Scaling", options= [False, True], index= 1)
+        input_pred_step_size = st.number_input(label= "Select Horizon Step", value= 2, min_value= 1, max_value= 4)
+        input_beta = st.number_input(label= "Select Beta", value= 0.8, min_value= 0.1, max_value= 1.0, step= 0.1)
 
     if input_pred_model == "LSTM":
-        pred_data = create_Prediction(filepath= FILENAME, model_name= "LSTM", scaling= True, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
+        pred_data = create_Prediction(filepath= FILENAME, model_name= "LSTM", scaling= input_pred_scaling, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
         inversed_predicitons = pred_data[0]
         inversed_targets = pred_data[1] 
         loss = pred_data[2]
@@ -387,7 +419,7 @@ with tab_pred:
         plot_predictions(x, inversed_targets, inversed_predicitons, loss)
 
     elif input_pred_model == "Transformer":    
-        pred_data = create_Prediction(filepath= FILENAME, model_name= "Transformer", scaling= True, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
+        pred_data = create_Prediction(filepath= FILENAME, model_name= "Transformer", scaling = input_pred_scaling, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
         predictions = pred_data[0]
         targets = pred_data[1] 
         loss = pred_data[2]
@@ -396,8 +428,8 @@ with tab_pred:
         plot_predictions(x, targets, predictions, loss)
 
     elif input_pred_model == "Ensemble":    
-        pred_data_lstm = create_Prediction(filepath= FILENAME, model_name= "LSTM", scaling= True, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
-        pred_data_trsnf = create_Prediction(filepath= FILENAME, model_name= "Transformer", scaling= True, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
+        pred_data_lstm = create_Prediction(filepath= FILENAME, model_name= "LSTM", scaling = input_pred_scaling, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
+        pred_data_trsnf = create_Prediction(filepath= FILENAME, model_name= "Transformer", scaling = input_pred_scaling, start_date= input_pred_start_datetime, horizon_step= input_pred_step_size, beta= input_beta)
         predictions_lstm, targets_lstm, loss_lstm = pred_data_lstm[0], pred_data_lstm[1], pred_data_lstm[2]
         predictions_trsnf, targets_trsnf, loss_trnsf = pred_data_trsnf[0], pred_data_trsnf[1], pred_data_trsnf[2]
         ensemble = create_ensemble([predictions_lstm, predictions_trsnf], targets_lstm)
